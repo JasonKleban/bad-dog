9c205af315893591e4e24171b3856922484eb850
Jason Kleban <jason.kleban@outlook.com>
2017-04-21 17:13:32 -0400

    directory listings and files are served up

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express    = require('express');
 var serveIndex = require('serve-index');
+var appRoot = require('app-root-path');
 
 var app = express();
+var images = appRoot.resolve('out');
 
 // Serve URLs like /ftp/thing as public/ftp/thing
-app.use('./out', serveIndex('/', {'icons': true}));
+app.use(express.static(images));
+app.use('/', serveIndex(images, {'icons': true}));
+
+console.log(images);
+
 var listener = app.listen(8888, () => {
     console.log('http://localhost:' + listener.address().port);
 });
