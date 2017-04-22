b77b1e4c22a8601b24f9f249b0b06e9dcee621fc
Jason Kleban <jkleban@outlook.com>
2017-04-22 09:25:44 -0400

    in memory image

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,14 @@
-import os
+from time import sleep
+from picamera import PiCamera
 import time
+timestr = time.strftime("%Y%m%d-%H%M%S")
 
-FRAMES = 1000
-TIMEBETWEEN = 16
+camera = PiCamera()
 
-frameCount = 0
-while frameCount < FRAMES:
-    imageNumber = str(frameCount).zfill(7)
-    os.system("raspistill -o out/image%s.jpg"%(imageNumber))
-    frameCount += 1
-    time.sleep(TIMEBETWEEN - 6) #Takes roughly 6 seconds to[url][/url] take a picture
+# Create the in-memory stream
+stream = BytesIO()
+camera.capture(stream, format='png')
+# "Rewind" the stream to the beginning so we can read its content
+stream.seek(0)
+image = Image.open(stream)
+image.save('out/' + time.strftime('%Y%m%d-%H%M%S') + '.png','PNG')
