8cff5dea5e911b4086a3048a6c10cd501de412f5
Jason Kleban <jkleban@outlook.com>
2017-04-22 10:41:17 -0400

    fixing None image first iteration

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,8 @@
-from time import sleep
-from picamera import PiCamera
 import time
 import io
-from PIL import Image
 import numpy as np
+from picamera import PiCamera
+from PIL import Image
 #from skimage.measure import structural_similarity as ssim
 
 def mse(imageA, imageB):
@@ -27,13 +26,20 @@ with PiCamera() as camera:
         stream.seek(0)
         image = Image.open(stream)
         imageArray = np.asarray(image.convert('L'))
+
+        filename = 'out/' + time.strftime('%Y%m%d-%H%M%S') + '.png'
+
+        mseResult = None
+
         if lastImage is None:
+            image.save(filename, 'PNG')
+            lastImage = imageArray
+        else:
             mseResult = mse(lastImage, imageArray)
             if similarity < mseResult:
-                filename = 'out/' + time.strftime('%Y%m%d-%H%M%S') + '.png'
-                image.save(filename,'PNG')
+                image.save(filename, 'PNG')
                 lastImage = imageArray
                 print('kept {} with mse of {}'.format(filename, mseResult))
             else:
-                print('mse was only {}'.format(mseResult))
+                print('image discarded as mse was only {}'.format(mseResult))
         time.sleep(10)
