869482f9c5185b94a0b35c69bca66161f7986be4
Jason Kleban <jkleban@outlook.com>
2017-04-22 09:46:44 -0400

    skips redundant scenes

--- src/__main__.py
+++ src/__main__.py
@@ -1,16 +1,31 @@
 from time import sleep
 from picamera import PiCamera
 import time
-from io import BytesIO
+import io
 from PIL import Image
-timestr = time.strftime("%Y%m%d-%H%M%S")
+import numpy as np
 
-camera = PiCamera()
+def mse(imageA, imageB):
+    # the 'Mean Squared Error' between the two images is the
+    # sum of the squared difference between the two images;
+    # NOTE: the two images must have the same dimension
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    err /= float(imageA.shape[0] * imageA.shape[1])
 
-# Create the in-memory stream
-stream = BytesIO()
-camera.capture(stream, format='png')
-# "Rewind" the stream to the beginning so we can read its content
-stream.seek(0)
-image = Image.open(stream)
-image.save('out/' + time.strftime('%Y%m%d-%H%M%S') + '.png','PNG')
+    # return the MSE, the lower the error, the more "similar"
+    # the two images are
+    return err
+
+lastImage = None
+similarity = 0.1
+stream = io.BytesIO()
+
+with PiCamera() as camera:
+    for foo in camera.capture_continuous(stream, format='png'):
+        stream.truncate()
+        stream.seek(0)
+        image = Image.open(stream)
+        if lastImage is None or similarity < mse(lastImage, image):
+            image.save('out/' + time.strftime('%Y%m%d-%H%M%S') + '.png','PNG')
+            lastImage = image
+        time.sleep(10)
