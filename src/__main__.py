1171fbbf4b5848f3aa87997ef4223faa52721f10
Jason Kleban <jkleban@outlook.com>
2017-04-22 11:05:07 -0400

    different mse implementation?

--- src/__main__.py
+++ src/__main__.py
@@ -1,20 +1,16 @@
 import time
 import io
-import numpy as np
+import math, operator
+import ImageChops
 from picamera import PiCamera
 from PIL import Image
 #from skimage.measure import structural_similarity as ssim
 
-def mse(imageA, imageB):
-    # the 'Mean Squared Error' between the two images is the
-    # sum of the squared difference between the two images;
-    # NOTE: the two images must have the same dimension
-    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-    err /= float(imageA.shape[0] * imageA.shape[1])
-
-    # return the MSE, the lower the error, the more "similar"
-    # the two images are
-    return err
+def rmsdiff(im1, im2):
+    h = ImageChops.difference(im1, im2).histogram()
+    return math.sqrt(reduce(operator.add,
+        map(lambda h, i: h*(i**2), h, h.keys())) / 
+            (float(im1.size[0]) * im1.size[1]))
 
 lastImage = None
 similarity = 200
@@ -25,23 +21,20 @@ with PiCamera() as camera:
         stream.truncate()
         stream.seek(0)
         image = Image.open(stream)
-        imageArray = np.asarray(image.convert('L')).flatten()
 
         filename = 'out/' + time.strftime('%Y%m%d-%H%M%S') + '.png'
 
-        mseResult = None
+        diff = None
 
         if lastImage is None:
             image.save(filename, 'PNG')
-            lastImage = imageArray
+            lastImage = image
         else:
-            mseResult = mse(lastImage, imageArray)
-            if similarity < mseResult:
+            diff = rmsdiff(lastImage, image)
+            if similarity < diff:
                 image.save(filename, 'PNG')
-                lastImage = imageArray
-                print('kept {} with mse of {}'.format(filename, mseResult))
+                lastImage = image
+                print('kept {} with rms of {}'.format(filename, diff))
             else:
-                print(lastImage)
-                print(imageArray)
-                print('image discarded as mse was only {}'.format(mseResult))
+                print('image discarded as rms was only {}'.format(diff))
         time.sleep(10)
