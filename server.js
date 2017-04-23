c81f2d69a8f8fe275981d8a1fc2d5f6f40918d70
Jason Kleban <jkleban@outlook.com>
2017-04-23 19:16:22 -0400

    trying to add in the labeler

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
 var express    = require('express');
 var serveIndex = require('serve-index');
 var appRoot = require('app-root-path');
+const fs = require('fs');
 
 var app = express();
-var images = appRoot.resolve('out');
+var images = appRoot.resolve('out/capturedData');
 
-// Serve URLs like /ftp/thing as public/ftp/thing
-app.use(express.static(images));
-app.use('/', serveIndex(images, {'icons': true}));
+app.use(express.static(appRoot.resolve('out')));
+app.use('/capturedData', serveIndex(images, {'icons': true}));
 
-console.log(images);
+app.get('/list', function(req, res){
+    fs.readdir(images, (err, files) => {
+        var pngs = [];
+        files.forEach(function (file) {
+            if (file.indexOf('.png') != -1) {
+                pngs.push(file);
+            }
+        });
+        res.send(pngs);
+    });
+});
+
+app.post('/good/:name', function(req, res){
+    fs.rename(images + req.query.name, images + '/good/' + req.query.name, (err) => {
+        if (err) res.status(500).send(err);
+        else res.send('OK');
+    });
+});
+
+app.post('/bad/:name', function(req, res){
+    fs.rename(images + req.query.name, images + '/bad/'  + req.query.name, (err) => {
+        if (err) res.status(500).send(err)
+        else res.send('OK');
+    });
+});
 
 var listener = app.listen(8888, () => {
     console.log('http://localhost:' + listener.address().port);
