5ac014c12157dbfa134b8d97d7006d10e5ca2529
Jason Kleban <jkleban@outlook.com>
2017-04-27 21:28:13 -0400

    fixes

--- websrc/app.ts
+++ websrc/app.ts
@@ -1,5 +1,5 @@
 
-var fileList : string[] = [ 'https://ashleyfurniture.scene7.com/is/image/AshleyFurniture/68902-38-10x8-CROP?$AFHS-Grid-1X$' ];
+var fileList : string[] = [];
 var headFile : string;
 
 var nextImage = () => {
@@ -10,14 +10,14 @@ var nextImage = () => {
 };
 
 $('#good-dog').on('click', () => {
-    $.get('/good/' + headFile)
+    $.post('/good/' + headFile)
     .done((data : string[]) => {
         nextImage();
     });
 });
 
 $('#bad-dog').on('click', () => {
-    $.get('/bad/' + headFile)
+    $.post('/bad/' + headFile)
     .done((data : string[]) => {
         nextImage();
     });
@@ -30,46 +30,3 @@ $(() => {
         nextImage();
     });
 });
-
-//     $("#tinderslide").jTinder({
-//         // dislike callback
-//         onDislike: function (item) {
-//             console.log(item);
-//             $.post(`/bad/${$(item).prop('fileName')}`)
-//             .done(() => {
-//                 $('#status').text('Ok');
-//                 preparePane();
-//             })
-//             .fail(err => {
-//                 $('#status').text(err);
-//             });
-//         },
-//         // like callback
-//         onLike: function (item) {
-//             console.log(item);
-//             $.post(`/good/${$(item).prop('fileName')}`)
-//             .done(() => {
-//                 $('#status').text('Ok');
-//                 preparePane();
-//             })
-//             .fail(err => {
-//                 $('#status').text(err);
-//             });
-//         },
-//         animationRevertSpeed: 200,
-//         animationSpeed: 400,
-//         threshold: 1,
-//         likeSelector: '.like',
-//         dislikeSelector: '.dislike'
-//     });
-
-//     /**
-//      * Set button action to trigger jTinder like & dislike.
-//      */
-//     $('.actions .like, .actions .dislike').click(function(e){
-//         e.preventDefault();
-//         $("#tinderslide").jTinder($(this).attr('class'));
-//     });
-
-
-// });
