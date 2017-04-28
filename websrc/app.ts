ea633733ddf67986ec2fe4761027166005dfc907
Jason Kleban <jkleban@outlook.com>
2017-04-27 21:23:27 -0400

    jTinder wasn't working.  back to basics!

--- websrc/app.ts
+++ websrc/app.ts
@@ -1,83 +1,75 @@
-/// <reference path="../node_modules/@types/jquery/index" />
+
+var fileList : string[] = [ 'https://ashleyfurniture.scene7.com/is/image/AshleyFurniture/68902-38-10x8-CROP?$AFHS-Grid-1X$' ];
+var headFile : string;
+
+var nextImage = () => {
+    headFile = fileList.pop();
+
+    $('#capture')
+        .attr('src', `capturedData/${headFile}`);
+};
+
+$('#good-dog').on('click', () => {
+    $.get('/good/' + headFile)
+    .done((data : string[]) => {
+        nextImage();
+    });
+});
+
+$('#bad-dog').on('click', () => {
+    $.get('/bad/' + headFile)
+    .done((data : string[]) => {
+        nextImage();
+    });
+});
 
 $(() => {
-    var paneClasses = [
-        'pane1',
-        'pane2',
-        'pane3',
-        'pane4',
-        'pane5'
-    ];
-
-    var fileList : string[];
-
-    var preparePane = () => {
-        var headFile = fileList.pop();
-
-        var slide = $(`<li class="${paneClasses[0]}">
-                <div class="img"></div>
-                <div>${headFile}</div>
-                <div class="like"></div>
-                <div class="dislike"></div>
-            </li>`)
-            .prop('fileName', headFile);
-        
-        $('.img', slide)
-        .css('background', `url('capturedData/${headFile}') no-repeat scroll center center`);
-
-        $('#panes').prepend(slide);
-        
-        paneClasses.push(paneClasses.shift());
-    };
-
-    $("#tinderslide").jTinder({
-        // dislike callback
-        onDislike: function (item) {
-            console.log(item);
-            $.post(`/bad/${$(item).prop('fileName')}`)
-            .done(() => {
-                $('#status').text('Ok');
-                preparePane();
-            })
-            .fail(err => {
-                $('#status').text(err);
-            });
-        },
-        // like callback
-        onLike: function (item) {
-            console.log(item);
-            $.post(`/good/${$(item).prop('fileName')}`)
-            .done(() => {
-                $('#status').text('Ok');
-                preparePane();
-            })
-            .fail(err => {
-                $('#status').text(err);
-            });
-        },
-        animationRevertSpeed: 200,
-        animationSpeed: 400,
-        threshold: 1,
-        likeSelector: '.like',
-        dislikeSelector: '.dislike'
-    });
-
-    /**
-     * Set button action to trigger jTinder like & dislike.
-     */
-    $('.actions .like, .actions .dislike').click(function(e){
-        e.preventDefault();
-        $("#tinderslide").jTinder($(this).attr('class'));
-    });
-
     $.get('/list/')
     .done((data : string[]) => {
         fileList = data;
-
-        $('#panes').empty();
-
-        [0,1,2,3,4].forEach(fileInList => {
-            preparePane();
-        });
+        nextImage();
     });
 });
+
+//     $("#tinderslide").jTinder({
+//         // dislike callback
+//         onDislike: function (item) {
+//             console.log(item);
+//             $.post(`/bad/${$(item).prop('fileName')}`)
+//             .done(() => {
+//                 $('#status').text('Ok');
+//                 preparePane();
+//             })
+//             .fail(err => {
+//                 $('#status').text(err);
+//             });
+//         },
+//         // like callback
+//         onLike: function (item) {
+//             console.log(item);
+//             $.post(`/good/${$(item).prop('fileName')}`)
+//             .done(() => {
+//                 $('#status').text('Ok');
+//                 preparePane();
+//             })
+//             .fail(err => {
+//                 $('#status').text(err);
+//             });
+//         },
+//         animationRevertSpeed: 200,
+//         animationSpeed: 400,
+//         threshold: 1,
+//         likeSelector: '.like',
+//         dislikeSelector: '.dislike'
+//     });
+
+//     /**
+//      * Set button action to trigger jTinder like & dislike.
+//      */
+//     $('.actions .like, .actions .dislike').click(function(e){
+//         e.preventDefault();
+//         $("#tinderslide").jTinder($(this).attr('class'));
+//     });
+
+
+// });
